f3dd50b47ecd2541002f59ba6abacf82f6608aab
cho45 <cho45@lowreal.net>
2007-12-11 15:18:00 +0000

    lang/javascript/blosxom.rhino/blosxom.rhino.js:     コードのクリーンアップ
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/blosxom.rhino@3056 d0d07461-0603-4401-acd4-de1884942a52

--- blosxom.rhino.js
+++ blosxom.rhino.js
@@ -25,7 +25,7 @@ BlosxomRhino.prototype = {
 			return b.datetime.valueOf() - a.datetime.valueOf();
 		});
 
-		var path_info = this.getenv("PATH_INFO") || "/intro";
+		var path_info = this.getenv("PATH_INFO") || "/";
 		if (path_info.match(RegExp("^/(\\d{4})(?:/(\\d\\d)(?:/(\\d\\d))?)?"))) {
 			var year = +RegExp.$1, month = RegExp.$2 - 1, day = +RegExp.$3;
 			entries = entries.filter(function (i) {
@@ -34,7 +34,6 @@ BlosxomRhino.prototype = {
 					{k:"getMonth", v:month},
 					{k:"getDate",v:day}
 				].every(function (j) {
-					// p([j.k, i.datetime[j.k](), j.v]);
 					return (j.v <= 0) || i.datetime[j.k]() == j.v
 				});
 			});
@@ -53,9 +52,6 @@ BlosxomRhino.prototype = {
 
 
 		var template = new EJS(this._readLines("template.html").join("\n"));
-//		p(entries);
-//		return;
-
 		System.out.println(template.run({
 			title       : this.config.title,
 			author      : this.config.author,
@@ -64,9 +60,6 @@ BlosxomRhino.prototype = {
 			server_root : "http://" + this.getenv("SERVER_NAME"),
 			entries     : entries,
 		}));
-		System.out.println("あああ");
-		System.out.println(System.getProperty("file.encoding"));
-
 	},
 
 	getEntries : function () {
@@ -114,32 +107,6 @@ BlosxomRhino.prototype = {
 		return String(System.getenv(name) || "");
 	},
 };
-
-//print("Content-Type: text/plain");
-//print("");
-//p(String(System.getenv("USER")));
-
-
-//var files = File(Config.data_dir).listFiles(
-//	FilenameFilter({
-//		accept: function (dir, name) {
-//			return (name.match(/\.txt$/))
-//		}
-//	})
-//);
-//
-//p(files);
-//
-//var entries = files.map(function (i) {
-//	p(i.lastModified());
-//	p(i.getName());
-//	return {
-//		file : i,
-//		name : String(i).replace(RegExp("^"+Config.data_dir+"|\\..*$", "g"), ""),
-//		datetime : new Date(i.lastModified()),
-//	};
-//});
-//p(entries);
 
 new BlosxomRhino({
 	title    : "Blosxom.Rhino!",
